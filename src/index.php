11a772bd9956f4fd863347f5027835f221f027d5
Linkon Miyan <rudlinkon@gmail.com>
2022-11-20 09:48:00 +0600

    PR Testing Commit

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * PR Testing Commit
  * Note: this file exists only to remind developers to build the assets.
  * For the real index.php that gets built and boots WordPress,
  * please refer to _index.php.
